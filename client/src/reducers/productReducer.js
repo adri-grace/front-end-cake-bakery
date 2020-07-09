19e2e5e94622702d1eccf1d3428e9dded904c691
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-07-09 13:12:18 -0500

    set up store, reducers and actions

---
+++ client/src/reducers/productReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  products:[]
+}
+
+const initialState = (state = initialState, action) => {
+  switch(action.type)
+  default:
+  return state
+
+}
+export default productReducer
